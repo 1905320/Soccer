a88e19ea8ebc1029025ca9114ff6b3ce75daac4b
kameronlee <159819282+kameronlee@users.noreply.github.com>
2024-05-06 10:09:05 -0400

    Add files via upload

---
+++ 6-Black-Pong/ArrowkeySquare.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+
+//InflatingCircle extends YOUR Circle class and implements Animatable
+
+//Animatable requires a step() method, which is in this file, and a
+//drawMe(Graphics g) method, which you should already have in Circle
+
+public class ArrowkeySquare extends Square implements Animatable
+{
+   private int dX;
+   private int dY;
+      
+   // constructors
+   public ArrowkeySquare()
+   {
+      super(3, 80, 25, 200, Color.WHITE);
+      dX = 0;
+      dY = 0;
+   }
+   
+   public ArrowkeySquare(int sideValue, int sideValuey, int xValue, int yValue, Color cValue)
+   {
+      super(sideValue,sideValuey, xValue, yValue, cValue);
+      dX = 0;
+      dY= 0;
+   }
+   
+   //accessors
+   public int getDX()
+   {
+      return dX;
+   }
+   public int getDY()
+   {
+      return dY;
+   }
+   
+   //modifiers
+   public void setDX(int dXValue)
+   {
+      dX = dXValue;
+   }
+   public void setDY(int dYValue)
+   {
+      dY = dYValue;
+   }
+   
+      
+   //instance methods
+   public void step()  //Implement Animatable's required step()
+   {
+      setX(getX() + dX); 
+      setY(getY() + dY); 
+   }
+}
